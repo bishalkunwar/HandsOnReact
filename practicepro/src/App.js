a2659c25e9b37613e76f7d7352078f1a09875ab5
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-03 11:04:37 -0400

    Reusable Component Demo with the Use of Props

---
+++ practicepro/src/App.js
@@ -0,0 +1,12 @@
+import Main from "./Components/ReusableCompTry/Main";
+
+function App() {
+  return (
+    <div className="App">
+        <p>Hello World</p>  
+        <Main/>    
+    </div>
+  );
+}
+
+export default App;
---
+++ practicepro/src/Components/ReusableCompTry/Main.js
@@ -0,0 +1,20 @@
+export default function Main(){
+    return(
+        <div className="main" style={{display:"flex", flexDirection:"row", alignItems:"center", margin: "0px auto", padding: "20px 20px 20px 20px"}}>
+            <Contents image="hello.png" title="hello" desc="ram ram ram"/>
+            <Contents image="hello.png" title="hello" desc="ram ram ram"/>
+            <Contents image="hello.png" title="hello" desc="ram ram ram"/>
+        </div>
+
+    )
+}
+
+function Contents({image, title, desc}){
+    return(
+        <div className="main-container" style={{border:"1px solid black", display: "flex", flexWrap: "wrap", flexDirection: "column", textAlign: "center"}}>
+            <img src={image} alt="img" />
+            <h1>{title}</h1>
+            <p>{desc}</p>
+    </div>
+    );
+};
