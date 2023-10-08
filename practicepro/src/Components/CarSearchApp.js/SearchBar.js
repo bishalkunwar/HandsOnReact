097883faad636f941d76b3270f1f223c59f233b4
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-07 21:12:10 -0400

    Handling input elements

--- practicepro/src/Components/CarSearchApp.js/SearchBar.js
+++ practicepro/src/Components/CarSearchApp.js/SearchBar.js
@@ -1,16 +1,23 @@
+import {useState} from 'react';
 
 export default function SearchBar({onSubmit}){
     
+    const[term, setTerm] = useState('');
+
     const handleFormSubmit = (e) => {
         e.preventDefault();
         // console.log("I need to tell something to my parent");
         onSubmit("cars");
     }
 
+    const handleChange = (e) => {
+        setTerm(e.target.value);
+    }
+
     return(
         <div>
             <form onSubmit={handleFormSubmit}>
-                <input />
+                <input value={term} onChange={handleChange}/>
             </form>
         </div>
     );
