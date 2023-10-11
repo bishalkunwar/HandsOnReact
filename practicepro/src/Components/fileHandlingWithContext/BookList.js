10c60b65f7cef999a619ef384ccc59843ecb6da7
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-11 13:54:08 -0400

    getting started with context

--- practicepro/src/Components/fileHandlingWithContext/BookList.js
+++ practicepro/src/Components/fileHandlingWithContext/BookList.js
@@ -1,13 +1,19 @@
+import { useContext } from "react";
+import BooksContext from "../../context/books";
 import BookShow from "./BookShow";
 
 function BookList({books, onDelete, onUpdate}){
+    const value = useContext(BooksContext);
     const renderedBooks = books.map((book)=>{
         console.log(book.title);
         return <BookShow key={book.id} book={book} onDelete={onDelete} onUpdate={onUpdate}/>
     });
 
     return(
-        <div className="book-list">{renderedBooks}</div>
+        <div className="book-list">
+            {value}
+            {renderedBooks}
+        </div>
     );
 };
 
--- practicepro/src/index.js
+++ practicepro/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import searchImages from './api';
+import BooksContext from './context/books';
+// import searchImages from './api';
 
-searchImages();
+// searchImages();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BooksContext.Provider value={4}>
+      <App/>
+    </BooksContext.Provider>
   </React.StrictMode>
 );
 
