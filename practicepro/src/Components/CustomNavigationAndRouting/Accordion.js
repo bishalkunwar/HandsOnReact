20110c9a25a3d87e0d49f5bb6a86da160d98ceb4
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-13 14:23:43 -0400

    conditional rendering of list items done with handleClick at accordion

--- practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 export default function Accordion({items}){
 
-    const[expandedIndex, seExpandedIndex] = useStateState(0);
+    const[expandedIndex, setExpandedIndex] = useState(0);
 
     const renderedItems = items.map((item, index)=>{
-    const isExpanded = index===expandedIndex;
+        const isExpanded = index===expandedIndex;
         const content = isExpanded&&<div>{item.content}</div>
-    
+        
+        const handleClick = ()=>{
+            setExpandedIndex(index)
+        }
 
        return(
         <div key={item.id}>
-            <div>{item.label}</div>
+            <div onClick={handleClick}>{item.label}</div>
             {content}
         </div>
        )
