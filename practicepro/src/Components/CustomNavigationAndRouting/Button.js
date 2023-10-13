182da266f2c28f119b92d7fcfe596fa63b0adf3f
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-12 21:01:36 -0400

    optional props passing to buttons elements from parent to child via spread operator

--- practicepro/src/Components/CustomNavigationAndRouting/Button.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Button.js
@@ -1,7 +1,7 @@
 // import PropTypes from 'prop-types';
 import className from 'classnames';
 
-export default function Button({children, primary, secondary, success, warning, danger, outline, rounded }){
+export default function Button({children, primary, secondary, success, warning, danger, outline, rounded, ...rest }){
 
     const classes = className(' flex items-center px-3 py-1.5 border', {
         'border-blue-500 bg-blue-500 text-white':primary,
@@ -13,8 +13,9 @@ export default function Button({children, primary, secondary, success, warning, 
         'border-teal-400':outline,
 
     });
+
     return(
-        <button className={classes}>
+        <button className={classes} {...rest}>
             {children}
         </button>
     );
--- practicepro/src/Components/CustomNavigationAndRouting/Home.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Home.js
@@ -1,9 +1,17 @@
 import Button from "./Button";
 import {GoBell} from 'react-icons/go';
 export default function Home(){
+
+    const handleClick = ()=>{
+        alert("button clicked,")
+    }
+
+    const handleMouse = ()=>{
+        alert("hi");
+    };
     return(
         <div>
-            <Button primary rounded outline><GoBell/> Click me</Button><br/><br/>
+            <Button primary rounded outline onClick={handleClick}><GoBell/> Click me</Button><br/><br/>
             <Button secondary rounded outline>Click me</Button><br/><br/>
             <Button success rounded outline>Click me</Button><br/><br/>
             <Button warning rounded outline>Click me</Button><br/><br/>
