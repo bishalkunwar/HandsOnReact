cbbcf4ebb5d5c21fa560b6bd48231ce187926152
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-12 20:47:08 -0400

    some buttons with tailwind css

--- practicepro/src/Components/CustomNavigationAndRouting/Button.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Button.js
@@ -1,8 +1,20 @@
 // import PropTypes from 'prop-types';
+import className from 'classnames';
+
 export default function Button({children, primary, secondary, success, warning, danger, outline, rounded }){
 
+    const classes = className(' flex items-center px-3 py-1.5 border', {
+        'border-blue-500 bg-blue-500 text-white':primary,
+        'border-gray-500 bg-gray-500 text-white':secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-yellow-500 bg-yellow-500 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+        'rounded-full':rounded,
+        'border-teal-400':outline,
+
+    });
     return(
-        <button className="px-3 py-1.5 border border-blue-600 bg-blue-400 text-white" onClick={()=>{alert("Hello button");}}>
+        <button className={classes}>
             {children}
         </button>
     );
--- practicepro/src/Components/CustomNavigationAndRouting/Home.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Home.js
@@ -1,11 +1,13 @@
 import Button from "./Button";
-
+import {GoBell} from 'react-icons/go';
 export default function Home(){
     return(
         <div>
-            <Button>Click me</Button>
-            <Button>Click me</Button>
-            <Button>Click me</Button>
+            <Button primary rounded outline><GoBell/> Click me</Button><br/><br/>
+            <Button secondary rounded outline>Click me</Button><br/><br/>
+            <Button success rounded outline>Click me</Button><br/><br/>
+            <Button warning rounded outline>Click me</Button><br/><br/>
+            <Button danger rounded outline>Click me</Button><br/><br/>
         </div>
         
     );
