3b041f30db829aab2074222ee9b142297529249f
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-13 14:10:08 -0400

    trying out some conditional rendering, has some bugs and is incomplete for now at accordion

--- practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
@@ -1,15 +1,22 @@
-
+import { useState } from "react";
 
 export default function Accordion({items}){
 
-    const renderedItems = items.map((item)=>{
-        return(
-            <div key={item.id}>
-                <div>{item.label}</div>
-                <div>{item.content}</div>
-            </div>
-        );
+    const[expandedIndex, seExpandedIndex] = useStateState(0);
+
+    const renderedItems = items.map((item, index)=>{
+    const isExpanded = index===expandedIndex;
+        const content = isExpanded&&<div>{item.content}</div>
+    
+
+       return(
+        <div key={item.id}>
+            <div>{item.label}</div>
+            {content}
+        </div>
+       )
     });
+
     return(
         <div>
             {renderedItems}
