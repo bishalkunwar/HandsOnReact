14e3bb521b49bc6ee9567a50bd8cb407c6bc0aa7
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-13 14:32:20 -0400

    conditional rendering with handleClick event with mixture of shorthand and longhand function

--- practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
@@ -4,17 +4,18 @@ export default function Accordion({items}){
 
     const[expandedIndex, setExpandedIndex] = useState(0);
 
+    // this function and function called into the jsx is the combination of shorthand and longhand function.
+    const handleClick = (itemIndex)=>{
+        setExpandedIndex(itemIndex);
+    }
+
     const renderedItems = items.map((item, index)=>{
         const isExpanded = index===expandedIndex;
         const content = isExpanded&&<div>{item.content}</div>
-        
-        const handleClick = ()=>{
-            setExpandedIndex(index)
-        }
 
        return(
         <div key={item.id}>
-            <div onClick={handleClick}>{item.label}</div>
+            <div onClick={()=>handleClick(index)}>{item.label}</div>
             {content}
         </div>
        )
