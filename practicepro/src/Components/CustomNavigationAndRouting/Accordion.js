9eda808fff5bfab1a2808e7427d74b49af0bd2f2
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-13 22:42:23 -0400

    Applied some tailwind css to the conditional redering component elements

--- practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Accordion.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import {GoChevronDown, GoChevronLeft} from 'react-icons/go'
 export default function Accordion({items}){
 
     const[expandedIndex, setExpandedIndex] = useState(0);
@@ -11,13 +11,13 @@ export default function Accordion({items}){
 
     const renderedItems = items.map((item, index)=>{
         const isExpanded = index===expandedIndex;
-        const content = isExpanded&&<div>{item.content}</div>
+        const content = isExpanded&&<div className="border-b p-5">{item.content}</div>
 
-        const icon = <span>{isExpanded?'Down':'Left'}</span>
+        const icon = <span className="text-2xl">{isExpanded?<GoChevronDown/>:<GoChevronLeft/>}</span>
 
        return(
         <div key={item.id}>
-            <div onClick={()=>handleClick(index)}>{item.label}  {icon}</div>
+            <div className="flex p-5 bg-gray-100 border-b items-center cursor-pointer" onClick={()=>handleClick(index)}>{item.label}  {icon}</div>
             {content}
         </div>
        )
