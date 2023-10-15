4ad847eccd6455a0cbed0f655465761af8e50343
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-14 23:53:44 -0400

    A rough push of panel creation for reusable component creation

---
+++ practicepro/src/Components/CustomNavigationAndRouting/Panel.js
@@ -0,0 +1,9 @@
+import classNames from "classnames"
+
+export default function Panel({children, classNames, ...rest}){
+
+    const finalClassNames = classNames('');
+    return(
+        <p>this is a panel to try reusable components</p>
+    )
+}
