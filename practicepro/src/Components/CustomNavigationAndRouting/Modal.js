67efe1d50c61f06a952e1fd2e1781a12d92787a9
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-18 00:40:03 -0400

    some stylings

--- practicepro/src/Components/CustomNavigationAndRouting/Modal.js
+++ practicepro/src/Components/CustomNavigationAndRouting/Modal.js
@@ -7,17 +7,12 @@ export  default function Modal({onClose, actionBar, children}){
             <div onClick={onClose } className="absolute inset-0 bg-gray-300 opacity-80"></div>
             
             <div className="absolute inset-40 p-10 bg-white">
-<<<<<<< HEAD
                 <div className='flex flex-col justify-between h-full'>
                     {children} 
                     <div className='flex justify-end'>
                         {actionBar}
                     </div>
                 </div>
-=======
-                {children} 
-                {actionBar}
->>>>>>> 562e577c6d9d35006c49774662e58943653dad31
             </div>
         </div>,
         document.querySelector('.model-container')
