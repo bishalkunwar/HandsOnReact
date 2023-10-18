db6471662ab7a727bd6019e801cb18c10fd47d8d
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-17 22:59:35 -0400

    Creating a Modal Page

---
+++ practicepro/src/Components/CustomNavigationAndRouting/Modal.js
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+
+export  default function Modal(){
+    // const[expand, setExpand] = useState(0);
+ 
+    return(
+        <p>hello world</p>
+    );
+};
---
+++ practicepro/src/Components/Pages/ModalPage.js
@@ -0,0 +1,10 @@
+import Modal from "../CustomNavigationAndRouting/Modal";
+
+export default function ModalPage(){
+    return(
+
+        <div>
+            <Modal/>
+        </div>
+    );
+};
