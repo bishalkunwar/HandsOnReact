1cc9d38a50b4102e5351e6690e628048d68be9dd
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-18 01:57:38 -0400

    mapped the table data to be plot but has some bugs at array map method to be resolved

--- practicepro/src/Components/Pages/TablePage.js
+++ practicepro/src/Components/Pages/TablePage.js
@@ -1,7 +1,19 @@
 import Table from "../Table/Table";
 
 export default function TablePage(){
+    
+    const fruits = [
+        {name: 'Mango', color: 'Mango', score:10},
+        {name: 'Banana', color: 'Banana', score:9},
+        {name: 'Apple', color: 'Apple', score:8},
+        {name: 'Cherry', color: 'Cherry', score:7},
+        {name: 'Pineapple', color: 'Pineapple', score:6},
+    ]
+    
     return(
-        <Table/>
+        <div>
+            <Table fruits={fruits}/>
+        </div>
+        
     );
 };
--- practicepro/src/Components/Table/Table.js
+++ practicepro/src/Components/Table/Table.js
@@ -1,5 +1,29 @@
-export default function Table(){
+export default function Table({fruits}){
+    
+    const renderedData = fruits.map((fruit) => {
+        return(
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+
     return(
-        <p>Table</p>        
-    )
-}
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fruit</th>
+                        <th>Color</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderedData}
+                </tbody>
+            </table>
+        </div>
+    );
+};
