78e2ad742652aa656888ba71f09ee90e7fa5d5b7
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-27 13:50:03 -0400

    some progress with redux thunk

--- photoapp/src/components/UserLists.js
+++ photoapp/src/components/UserLists.js
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 export default function UserLists(){
     const dispatch = useDispatch();
 
+    const{isLoading, data, error} = useSelector((state)=>{
+        return state.users;
+    })
+
     useEffect(()=>{
         dispatch(fetchUsers());
     }, [dispatch]);
 
+    if(isLoading){
+        return <div>Loading...</div>
+    };
+
+    if(error){
+        return <div>Error...</div>
+    }
+
     return(
         <div>
-            <p>User's Lists.</p>
+            <p>{data.length}</p>
         </div>
     );
 };
