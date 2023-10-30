eb684ab816b299740de1e440aea7c2af583b1454
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-30 19:55:36 -0400

    createUser all done with the button onClick creating a user name with the help of faker, a styled box to list the users

--- photoapp/src/components/UserLists.js
+++ photoapp/src/components/UserLists.js
@@ -1,30 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser} from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 export default function UserLists(){
+    
+    const[isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const[loadingUsersError, setLoadingUsersError] = useState(null);
+    const[isCreatingUser, setIsCreatingUser] = useState(false);
+    const[creatingUserError, setCreatingUserError] = useState(null);
     const dispatch = useDispatch();
 
-    const{isLoading, data, error} = useSelector((state)=>{
+    const{data} = useSelector((state)=>{
         return state.users;
     })
 
     useEffect(()=>{
-        dispatch(fetchUsers());
+        setIsLoadingUsers(true);
+        dispatch(fetchUsers())
+            .unwrap()
+            // .then(()=>{
+            //     // console.log("Success");
+            //     setIsLoadingUsers(false);
+            // })
+            .catch((err)=>
+                // console.log("Fail");
+                setLoadingUsersError(err)
+            ).finally(()=>
+                setIsLoadingUsers(false) // finally is gonna get called anyway so we can remove the then function.
+            );
+
     }, [dispatch]);
 
     const handleAddUser = () => {
-        dispatch(addUser);
-        
+        setIsCreatingUser(true);
+        dispatch(addUser())
+            .unwrap()
+            // .then(())
+            .catch((error)=>setCreatingUserError(error))
+            .finally(()=>setIsCreatingUser(false));
     };
 
-    if(isLoading){
+    if(isLoadingUsers){
         return <Skeleton times={6} className="h-10 w-full"/>
     };
 
-    if(error){
+    if(loadingUsersError){
         return <div>Error...</div>
     }
 
@@ -42,7 +64,12 @@ export default function UserLists(){
                 <h1 className="m-2 text-xl">
                     Users
                 </h1>
-                <Button onClick={handleAddUser}>+ Add User</Button>
+                {
+                    isCreatingUser 
+                    ? 'Creating User' 
+                    : <Button onClick={handleAddUser}>+ Add User</Button>
+                }
+                {creatingUserError && 'Error Creating User'}
             </div>
             <div>{renderedUsers}</div>
         </div>
