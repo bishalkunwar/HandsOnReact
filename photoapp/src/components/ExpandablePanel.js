82ef6dd4d320ca7e5f8f698b226311f47d502633
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-31 12:25:33 -0400

    expanding and coalease panel with onClick event created

--- photoapp/src/components/ExpandablePanel.js
+++ photoapp/src/components/ExpandablePanel.js
@@ -1,14 +1,25 @@
-export default function ExpandablePanel({header, children}){
+import { useState } from "react";
+import {GoChevronDown, GoChevronLeft} from "react-icons/go";
 
+export default function ExpandablePanel({header, children}){
+    const[expanded, setExpanded] = useState(false);
+    
+    const handleExpandAndColease = () => {
+        setExpanded(!expanded);
+    }
+    
     return(
         <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex p-2 justify-between items-center">
                 <div className="flex flex-row items-center justify-between">
                     {header}
                 </div>
+                <div className="cursor-pointer" onClick={handleExpandAndColease}>
+                    {expanded ? <GoChevronDown/>:<GoChevronLeft/>}
+                </div>
             </div>
-            <div className="p-2 border-t">
-                {children}
+            <div onClick={handleExpandAndColease}>
+               {expanded && <div className="p-2 border-t"> {children} </div>}
             </div>
         </div>
     );
