830da78df6c0c75a543c69b9f6e471d3efb13774
Bishal487 <bishal.kunwar487@gmail.com>
2023-10-31 12:17:36 -0400

    Creating a reusable Expanding Panel for button  so that later on , click to expand internal content event

--- photoapp/src/components/UserListItem.js
+++ photoapp/src/components/UserListItem.js
@@ -2,6 +2,8 @@ import {GoTrash} from "react-icons/go";
 import Button from "./Button";
 import { deleteUser } from "../store";
 import useThunk from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import { Fragment } from "react";
 
 export default function UserListItem({user}){
 
@@ -10,19 +12,17 @@ export default function UserListItem({user}){
     const handleDeleteClick = () => {
         doDeleteUser(user);
     } 
+
+    const header = <>
+            <Button className="mr-3" loading={isLoadingToDelete} onClick={handleDeleteClick}>
+                <GoTrash/>
+            </Button>
+            
+            {errorWhileDeleting&& <div>Error Deleting User...</div> }
+            {user.name}
+        </>
     
     return(
-        <div key={user.id} className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                <div className="flex flex-row items-center justify-between">
-                    <Button className="mr-3" loading={isLoadingToDelete} onClick={handleDeleteClick}>
-                        <GoTrash/>
-                    </Button>
-                    
-                    {errorWhileDeleting&& <div>Error Deleting User...</div> }
-                    {user.name}
-                </div>
-            </div>
-        </div>
+        <ExpandablePanel header={header}>CONTENT</ExpandablePanel>
     );
 };
