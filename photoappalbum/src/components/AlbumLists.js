c767bbd722f12e7f78be52ced90a1ac7f49090b5
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-01 08:18:22 -0400

    rendering the album lists

--- photoappalbum/src/components/AlbumLists.js
+++ photoappalbum/src/components/AlbumLists.js
@@ -1,13 +1,37 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 export default function AlbumList({user}){
     
-    const {data, error, loading} = useFetchAlbumsQuery(user);
-    console.log(data, error, loading);
+    const {data, error, isLoading} = useFetchAlbumsQuery(user);
+    // console.log(data, error, loading);
+
+    let content;
+
+    if(isLoading){
+        content=<Skeleton times={3}/>
+    }else if(error){
+        content=<div>Error Loading Albums</div>
+    }else{
+        content = data.map(album=>{
+            const header = <div>{album.title}</div>;
+
+            return <ExpandablePanel key={album.id} header={header}>
+                Lists of photos in the album
+            </ExpandablePanel>
+        })
+    }
 
     return(
         <div>
-            albums for {user.name}
+            <div>
+             albums for {user.name}
+            </div>
+            <div>
+                {content}
+            </div>
         </div>
     );
 };
