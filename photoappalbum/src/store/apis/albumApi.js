20e6277c51bac5a953eedca1634ac91d81f0b6f6
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-02 21:13:28 -0400

    validating the albumApi tags

--- photoappalbum/src/store/apis/albumApi.js
+++ photoappalbum/src/store/apis/albumApi.js
@@ -10,7 +10,9 @@ const albumsApi = createApi({
    endpoints(builder){
       return{
          fetchAlbums: builder.query({
-            providesTags: ['albums'],
+            providesTags: (user, result, error)=>{ // eventhough we calling result and error we wont need them here for now.
+               return[{type: 'albums', id: user.id}];
+            },
             query: (user)=>{
                return{
                   url: '/albums',
@@ -23,7 +25,9 @@ const albumsApi = createApi({
          }),
 
          addAlbum: builder.mutation({
-            invalidatesTags: ['albums'],
+            invalidatesTags: (user, result, error)=>{
+               return[{type: 'albums', id: user.id}]
+            },
             query: (user)=>{
                return{
                   url: '/albums',
@@ -31,6 +35,17 @@ const albumsApi = createApi({
                   body: {
                      userId: user.id,
                      title: faker.commerce.productName(),                }
+               }
+            }
+         }),
+
+         deleteAlbun: builder.mutation({
+            providesTags: ['albums'],
+            query: (user)=>{
+               return{
+                  url: '/albums',
+                  method: 'DELETE',
+
                }
             }
          })
